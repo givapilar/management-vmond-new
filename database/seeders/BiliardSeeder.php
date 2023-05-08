2165aa6cbc1f6942b594ae74ee9225dcba6d39a6
syahrul0505 <syahrulalif1234@gmail.com>
2023-05-08 16:37:30 +0700

    add seeder dummy

---
+++ database/seeders/BiliardSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Biliard;
+use Illuminate\Database\Seeder;
+
+class BiliardSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new Biliard();
+        $user->nama = 'Meja B1';
+        $user->no_meja = '1';
+        $user->harga = '22.000,00';
+        $user->status = 'Tersedia';
+        $user->description = 'billiards, any of various games played on a rectangular table with a designated number of small balls and a long stick called a cue. The table and the cushioned rail bordering the table are topped with a feltlike tight-fitting cloth. Carom, or French,';
+
+        $user->save();
+
+        $user = new Biliard();
+        $user->nama = 'Meja B2';
+        $user->no_meja = '2';
+        $user->harga = '18.000,00';
+        $user->status = 'Tersedia';
+        $user->description = 'billiards, any of various games played on a rectangular table with a designated number of small balls and a long stick called a cue. The table and the cushioned rail bordering the table are topped with a feltlike tight-fitting cloth. Carom, or French,';
+
+        $user->save();
+
+        $user = new Biliard();
+        $user->nama = 'Meja B3';
+        $user->no_meja = '3';
+        $user->harga = '18.000,00';
+        $user->status = 'Tersedia';
+        $user->description = 'billiards, any of various games played on a rectangular table with a designated number of small balls and a long stick called a cue. The table and the cushioned rail bordering the table are topped with a feltlike tight-fitting cloth. Carom, or French,,';
+
+        $user->save();
+    }
+}
--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Restaurant;
 use Illuminate\Database\Seeder;
 
 class RestaurantSeeder extends Seeder
@@ -13,6 +14,31 @@ class RestaurantSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = new Restaurant();
+        $user->nama = 'Nasi Goreng';
+        $user->category = 'Makanan';
+        $user->harga = '22.000,00';
+        $user->status = 'Tersedia';
+        $user->description = 'Keripik Singkong Mercon adalah keripik dengan citarasa super pedas yang terbuat dari singkong. Keripik ini cocok buat kamu yang doyan dengan cemilan pedas. Tak hanya pedas, keripik ini ada dalam dua varian rasa, rasa gurih dan rasa manis.';
+
+        $user->save();
+
+        $user = new Restaurant();
+        $user->nama = 'Ayam Goreng';
+        $user->category = 'Makanan';
+        $user->harga = '18.000,00';
+        $user->status = 'Tersedia';
+        $user->description = 'Ayam goreng (bahasa Inggris: fried chicken), atau ayam goreng tepung adalah hidangan yang dibuat dari potongan daging ayam yang';
+
+        $user->save();
+
+        $user = new Restaurant();
+        $user->nama = 'Croffle';
+        $user->category = 'Makanan';
+        $user->harga = '18.000,00';
+        $user->status = 'Tersedia';
+        $user->description = 'The croffle has been the trendy breakfast and brunch pastry for some years now! But what is it? If you haven’t noticed it by its name,';
+
+        $user->save();
     }
 }
