a9d58d84abf8401407923797e63877f0f502020b
syahrul0505 <syahrulalif1234@gmail.com>
2024-02-03 23:23:31 +0700

    update report penjualan

--- app/Http/Controllers/ReportPenjualanController.php
+++ app/Http/Controllers/ReportPenjualanController.php
@@ -41,6 +41,7 @@ class ReportPenjualanController extends Controller
 
         $type = $request->has('type') ? $request->type : 'day';
         $user = $request->has('user_id') ? $request->user_id : 'All';
+        dd('masuk');
         if ($type == 'day') {
             dd('masuk1');
             $date = $request->has('start_date') ? $request->start_date : date('Y-m-d');
